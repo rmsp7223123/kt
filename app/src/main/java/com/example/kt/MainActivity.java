b4c99db725b68be9c994d6ac3a479015f829c184
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-11-28 20:59:25 +0900

    ++

--- app/src/main/java/com/example/kt/MainActivity.java
+++ app/src/main/java/com/example/kt/MainActivity.java
@@ -12,3 +12,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+class Solution {
+    public int solution(String t, String p) {
+        int length = t.length() - p.length() + 1;
+        long pNum = Long.parseLong(p);
+        int answer = 0;
+
+        for (int i = 0; i < length; i++) {
+            String temp = t.substring(i, i + p.length());
+
+            if (Long.parseLong(temp) <= pNum) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
