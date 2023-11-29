7e28418d323cfcf88b1f4dac47e182c6289e31dc
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-11-29 20:40:37 +0900

    ++

--- app/src/main/java/com/example/kt/MainActivity.java
+++ app/src/main/java/com/example/kt/MainActivity.java
@@ -29,3 +29,21 @@ class Solution {
         return answer;
     }
 }
+
+class Solution2 {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(k<goal.length){
+            if(i < cards1.length&&goal[k].equals(cards1[i])){
+                k++;
+                i++;
+            } else if(j < cards2.length&&goal[k].equals(cards2[j])){
+                k++;
+                j++;
+            } else break;
+        }
+        return k==goal.length ? "Yes" : "No";
+    };
+};
